25e32bd846b6ed6299a7655a5d5351f201095ff7
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-12-22 09:52:02 +0700

    Enhance Touch-Me game with random responses and update styles

--- Touch-Me/script.js
+++ Touch-Me/script.js
@@ -1,17 +1,43 @@
 const myBox = document.getElementById("myBox");
 const mybutton = document.querySelector("myButton");
+let count = 0;
+let lastNum = 0;
 
 myButton.addEventListener("click", (event) => {
-  myBox.style.backgroundColor = "tomato";
-  myBox.textContent = "OUCH! 😟"
+  let randomNum;
+
+  do {
+    randomNum = Math.floor(Math.random() * 4) + 1;
+  } while (randomNum == lastNum);
+
+  lastNum = randomNum;
+
+  switch (randomNum) {
+    case 1:
+      myBox.style.backgroundColor = "red";
+      myBox.textContent = "Ooouhh! 🥵";
+      break;
+    case 2:
+      myBox.style.backgroundColor = "orange";
+      myBox.textContent = "Stoppp! 😖";
+      break;
+    case 3:
+      myBox.style.backgroundColor = "darkred";
+      myBox.textContent = "OOouuff! 😵‍💫";
+      break;
+    case 4:
+      myBox.style.backgroundColor = "tomato";
+      myBox.textContent = "OUCH! 😟";
+      break;
+  }
 });
 
 myButton.addEventListener("mouseover", (event) => {
   myBox.style.backgroundColor = "yellow";
-  myBox.textContent = "Dont do it 😏"
+  myBox.textContent = "Dont do it 😏";
 });
 
 myButton.addEventListener("mouseout", (event) => {
-  myBox.style.backgroundColor = "lightGreen";
-  myBox.textContent = "Click Me 😃"
+  myBox.style.backgroundColor = "green";
+  myBox.textContent = "Click Me 😃";
 });
